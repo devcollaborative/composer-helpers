476e41814d78710003c0a3f334aa95a869ecd387
Jeff Hipp <jeff@devcollaborative.com>
2022-10-28 17:50:33 -0400

    WIP on checking drupal.org for module data.

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,6 +12,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 {
   protected $composer;
   protected $io;
+  protected $unsupported_modules = [];
 
   public function activate(Composer $composer, IOInterface $io) {
       $this->composer = $composer;
@@ -31,7 +32,44 @@ class Plugin implements PluginInterface, EventSubscriberInterface
   }
 
   public function checkDrupalStatus() {
-    var_dump($this->composer);
+    $repositoryManager = $this->composer->getRepositoryManager();
+    $localRepository = $repositoryManager->getLocalRepository();
+    $installationManager = $this->composer->getInstallationManager();
+    $packages = $localRepository->getPackages();
+
+    foreach($packages as $package) {
+      if ($package->getType() == 'drupal-module') {
+        if ($package->getName() == "drupal/ctools") {
+          $name= explode('/', $package->getName())[1];
+          $version = $package->getExtra()['drupal']['version'];
+
+          $module_data = simplexml_load_string(
+            file_get_contents("https://updates.drupal.org/release-history/$name/current")
+          );
+
+          $supported_versions= $module_data->supported_branches;
+
+          var_dump($supported_versions);
+
+          $release_data = $module_data->releases[0];
+
+          // var_dump($release_data);
+          foreach ($release_data as $release) {
+            // var_dump($release->version[0]);
+            if ($release->version[0] == $version) {
+              var_dump($release);
+            }
+          }
+          // [release][x][version]
+
+          // foreach ($module_data as $release) {
+          //   $version= $release->version;
+          //   var_dump($version);
+          // }
+          //
+        }
+      }
+    }
     // lando drush core:requirements --severity=2 --ignore=public:///.htaccess,entity_update,search_api_server_unavailable"
   }
 }
